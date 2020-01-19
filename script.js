1c196275bb3599698e126d372303f03871b6d076
Kaden Maguglin <57432402+Kadenmaguglin@users.noreply.github.com>
2020-01-18 22:20:22 -0800

    new script
    new

--- script.js
+++ script.js
@@ -1,21 +1,92 @@
-function generate(){
-    var length = document.getElementById("slider").value,
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%^&*()[]{}+=_-",
-    password = "";
-for (var i = 0, n = characters.length; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * n));
-}
+     
+    function setCaps(){
+        console.log("add caps");
+        capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        console.log(capitals);
+        document.getElementById("capsButton").style.backgroundColor = "lightgreen";
+        document.getElementById("capsButton").style.color = "black";
+        
+    }
+    function setLows(){
+        console.log("add lows");
+        lowercases = "abcdefghijklmnopqrstuvwxyz";
+        console.log(lowercases);
+        document.getElementById("lowsButton").style.backgroundColor = "lightgreen";
+        document.getElementById("lowsButton").style.color = "black";
+    }
+    function setNums(){
+        console.log("add nums");
+        numbers = "1234567890";
+        console.log(numbers);
+        document.getElementById("numsButton").style.backgroundColor = "lightgreen";
+        document.getElementById("numsButton").style.color = "black";
+    }
+    function setSymbs(){
+        console.log("add symbs");
+        symbols ="!@#$%^&*()[]{}+=_-"
+        console.log(symbols);
+        document.getElementById("symbsButton").style.backgroundColor = "lightgreen";
+        document.getElementById("symbsButton").style.color = "black";
+    } 
+     
+     function generate(){
 
-    document.getElementById("passwordField").value = password;
-    console.log(password);
-    console.log(document.getElementById("slider").value);
-}
+       if(typeof capitals === 'undefined'){
+            c="";
+        }else if(capitals === "ABCDEFGHIJKLMNOPQRSTUVWXYZ"){
+            c="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            console.log(c);
+        }else{
+            return;
+        }
+      
+
+        if(typeof lowercases === 'undefined'){
+         l="";
+        }else if(lowercases === "abcdefghijklmnopqrstuvwxyz"){
+            l="abcdefghijklmnopqrstuvwxyz";
+            console.log(l);
+        }else{
+            return;
+        }
+
+        if(typeof numbers === 'undefined'){
+            n="";
+        }else if(numbers === "1234567890"){
+            n="1234567890";
+            console.log(n);
+        }else{
+            return;
+        }
+
+        if(typeof symbols ==='undefined'){
+            s="";
+        }else if(symbols === "!@#$%^&*()[]{}+=_-"){
+            s="!@#$%^&*()[]{}+=_-";
+            console.log(s);
+        }else{
+            return;
+        }
+
+        var length = document.getElementById("slider").value,
+
+        characters = (c + l + n + s),
+        password = "";
+    for (var i = 0, n = characters.length; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * n));
+    }
+
+        document.getElementById("passwordField").value = password;
+        console.log(password);
+        console.log(document.getElementById("slider").value);
+        console.log(capitals + lowercases + numbers + symbols);
+    }
 
 
-function copy(){
-
-    var copyText=document.getElementById("passwordField");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-}
+    function copy(){
+   
+        var copyText=document.getElementById("passwordField");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+    }
